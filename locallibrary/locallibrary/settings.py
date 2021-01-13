df0620e793774b8f4a68c8851597632e7d0e8fd0
rajathbijoor <62019786+rajathbijoor@users.noreply.github.com>
2021-01-13 11:12:03 +0530

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,8 +145,8 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-db_frm_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_frm_env)
+#db_frm_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_frm_env)
 
 
 #STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
